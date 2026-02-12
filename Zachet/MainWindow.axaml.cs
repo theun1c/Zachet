83164e8ed2d5bc0cad899d71861b96b8273b28eb
theun1c <belovdima1122@yandex.ru>
2026-02-12 23:29:06 +0300

    initial commit

---
+++ Zachet/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Zachet
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
