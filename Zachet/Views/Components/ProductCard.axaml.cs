c2ae083438ef5a854765e38f865bf6593457a006
theun1c <belovdima1122@yandex.ru>
2026-03-05 02:47:17 +0300

    feat: add product cards; style: add card styles

---
+++ Zachet/Views/Components/ProductCard.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Zachet.Views;
+
+public partial class ProductCard : UserControl
+{
+    public ProductCard()
+    {
+        InitializeComponent();
+    }
+}
